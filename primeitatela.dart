4910ac20a3107b2da957848c853de0de784f930b
Kleyton Carv <135996798+kleytoncarv@users.noreply.github.com>
2023-12-03 22:36:57 -0300

    Update primeitatela.dart

--- primeitatela.dart
+++ primeitatela.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Minha Aplicação'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Função chamada quando o botão é pressionado
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Olá, mundo!'),
+                ),
+              );
+            },
+            child: Text('Pressione-me'),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
